5f2cdbe14a96c33da928475106f7a5b4659232e0
Sam Denty <samddenty+gh@gmail.com>
2019-05-21 19:28:51 +0100

    batched useForceUpdate

--- packages/react/src/hooks/useForceUpdate.ts
+++ packages/react/src/hooks/useForceUpdate.ts
@@ -1,9 +1,21 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 
 export const useForceUpdate = () => {
   const [_, setRerenders] = useState(0)
+  const timerRef = useRef<number>()
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(timerRef.current)
+    }
+  }, [])
 
   return () => {
-    setRerenders(r => r + 1)
+    if (timerRef.current) return
+
+    timerRef.current = setTimeout(() => {
+      setRerenders(r => r + 1)
+      timerRef.current = undefined
+    })
   }
 }
