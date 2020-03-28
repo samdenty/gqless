ca62f66d99c674347a3e0d713b515700d74325b4
Sam Denty <samddenty@gmail.com>
2020-03-28 18:21:01 +0000

    Fix input object nullability (#72)
    * fix #71
    
    * add changeset
    
    * fix csb
    
    * fix ci config
    
    * fix ci config

--- packages/schema/src/Codegen/files/generated/types.ts
+++ packages/schema/src/Codegen/files/generated/types.ts
@@ -221,11 +221,12 @@ export class TypesFile extends File {
   }
 
   public generateField(field: SchemaField, resolveType?: TypeResolver) {
+    const NULLABLE = field.type.nullable ? '?' : ''
     const fieldType = this.generateType(field.type, resolveType)
 
     if (field.args) this.import(CORE, this.names.FieldsTypeArg)
 
-    return `${this.generateFieldComments(field)}${field.name}: ${
+    return `${this.generateFieldComments(field)}${field.name}${NULLABLE}: ${
       field.args
         ? `${this.names.FieldsTypeArg}<${this.generateArgs(
             field.args
