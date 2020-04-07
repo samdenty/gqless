9fd1aa9346df77db445a0536a981f6f557e1b7d2
Daave <daave@widgetbot.io>
2020-04-07 13:14:48 +0100

    Fix calling queries with args

--- packages/schema/src/Codegen/files/generated/types.ts
+++ packages/schema/src/Codegen/files/generated/types.ts
@@ -227,12 +227,11 @@ export class TypesFile extends File {
   }
 
   public generateField(field: SchemaField, resolveType?: TypeResolver) {
-    const NULLABLE = field.type.nullable ? '?' : ''
     const fieldType = this.generateType(field.type, resolveType)
 
     if (field.args) this.import(CORE, this.names.FieldsTypeArg)
 
-    return `${this.generateFieldComments(field)}${field.name}${NULLABLE}: ${
+    return `${this.generateFieldComments(field)}${field.name}: ${
       field.args
         ? `${this.names.FieldsTypeArg}<${this.generateArgs(
             field.args
@@ -242,7 +241,7 @@ export class TypesFile extends File {
   }
 
   public generateType(type: Type, resolveType = this.typeReference): string {
-    const nullType = type.nullable ? '| null' : ''
+    const nullType = type.nullable ? '| undefined | null' : ''
 
     switch (type.kind) {
       case 'OBJECT':
